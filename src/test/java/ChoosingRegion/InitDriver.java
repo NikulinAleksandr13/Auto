70be9412c182305bd0d97db56967825738ddfe8a
Aleksandr Nikulin <nikulin@komus.net>
2023-01-23 10:27:58 +0300

    [Region] Добавил новый тест Region

---
+++ src/test/java/ChoosingRegion/InitDriver.java
@@ -0,0 +1,12 @@
+package ChoosingRegion;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InitDriver {
+    public static WebDriver getChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        return new ChromeDriver();
+
+    }
+}
